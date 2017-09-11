2abd3f5feb920300c58c94bce0d14c9f0af746fa
Luis Espinal <lespinalrivera@gmail.com>
2017-09-10 22:56:04 -0700

    first commit adding lines of code

---
+++ bot.js
@@ -0,0 +1,50 @@
+console.log("Hello user, the SCC bot is online")
+
+var Twit = require('twit');
+
+var config = require('./config');
+var T = new Twit(config);
+
+var tweet = {
+  status: 'Hi Community! What did you code today? #Surreycodes'
+}
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted(err, data, response) {
+  if (err) {
+    console.log('could not be tweeted');
+  } else {
+    console.log("I'm ALIVE!!");
+  }
+}
+
+// var retweet = function() {
+//   var params = {
+//     q: '#surreycode, #surreycodes, #surreycodecamp',
+//     result_type: 'recent',
+//     lang: 'en'
+//   }
+//
+//   T.get('search/tweets', params, gotData);
+//
+//   function gotData(err, data, response) {
+//     if (!err) {
+//       var retweetId = data.statuses[0].id_str;
+//       Twitter.post('statuses/retweet/:id', {
+//         id:retweetId
+//       }, function(err, response) {
+//         if (response) {
+//           console.log('Retweeted!');
+//         }
+//         if (err) {
+//           console.log('I Could not Retweet :( Something is wrong.')
+//         }
+//       });
+//     }
+//     else {
+//       console.log('uh oh, something went wrong while searching')
+//     }
+//   }
+//
+// }
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'X3if1JuP3A45HHkXS9RiFKggz',
+  consumer_secret:      '7tvIEM6ia3GDCDYH2RxYesR2RGqr2eVnMSwrgzgPTXBed0os8F',
+  access_token:         '825016803285880832-3IF2NRcFcjytu6iv2Qr4PvRS02YITJa',
+  access_token_secret:  'o1Tg2q7H0tQa5pnT07x34SBMHlGrYkjfbW3e4yBUP5Cbe',
+}
