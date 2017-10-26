4a46db11bd42f86a63fc9328ccfe578987fbccb3
Luis Espinal <lespinalrivera@gmail.com>
2017-10-25 20:00:26 -0700

    replacing old twitter keys, oops! messed it up

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
-  consumer_key:         'X3if1JuP3A45HHkXS9RiFKggz',
-  consumer_secret:      '7tvIEM6ia3GDCDYH2RxYesR2RGqr2eVnMSwrgzgPTXBed0os8F',
-  access_token:         '825016803285880832-3IF2NRcFcjytu6iv2Qr4PvRS02YITJa',
-  access_token_secret:  'o1Tg2q7H0tQa5pnT07x34SBMHlGrYkjfbW3e4yBUP5Cbe',
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 }
